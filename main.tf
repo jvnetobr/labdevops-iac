f4b0d5f68fb9e4b51b2772b23fa8a327945da126
José Vieira da Costa Neto <jose@localhost.localdomain>
2024-02-14 00:21:14 -0300

    updating main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  user_data = <<-EOF
-                #!/bin/bash
-            cd /home/ubuntu
-            echo "<h1>Feito com Terraform</h1>" > index.html
-            nohup busybox httpd -f -p 8080 &
-                EOF
+#  user_data = <<-EOF
+#                #!/bin/bash
+#            cd /home/ubuntu
+#            echo "<h1>Feito com Terraform</h1>" > index.html
+#            nohup busybox httpd -f -p 8080 &
+#                EOF
   tags = {
     Name = "Primeira instancia"
   }
