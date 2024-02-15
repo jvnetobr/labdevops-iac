748cfff7aba7f5e4b496c5b5aaeec9d13f6d5239
José Vieira da Costa Neto <jose@localhost.localdomain>
2024-02-15 00:32:52 -0300

    updating main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "app_server" {
 #            nohup busybox httpd -f -p 8080 &
 #                EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Terraform Ansible Python"
   }
 }
